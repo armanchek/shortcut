3339325fb539512b8b1250cdf015b3ca67148b04
armanchek <153510092+armanchek@users.noreply.github.com>
2024-12-06 13:29:51 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "url-shortener",
+  "version": "1.0.0",
+  "description": "Simple URL shortener using Node.js, Express, and Redis",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": ["url", "shortener", "express", "redis"],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "ejs": "^3.1.10",
+    "express": "^4.21.1",
+    "nanoid": "^5.0.9",
+    "redis": "^4.7.0"
+  }
+}
